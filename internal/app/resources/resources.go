c67874d6b19d69a5897d50423bfe7ab4383cd484
Jaga Santagostino <jagasantagostino@gmail.com>
2019-12-20 10:19:44 +0100

    wip

--- internal/app/resources/resources.go
+++ internal/app/resources/resources.go
@@ -4,8 +4,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-// User is a yser saved inside ETCD as a Custom Resouce (CRD api)
-
 type ResourcesService interface {
 	UserService
 	GetNamespaces() (names []string, err error)
@@ -16,7 +14,7 @@ type resourcesService struct {
 }
 
 func NewResourcesService(kc kubernetes.Interface) ResourcesService {
-	return &kubernetesResourcesService{
+	return &resourcesService{
 		kubeclient: kc,
 	}
 }
